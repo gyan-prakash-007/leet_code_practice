31fd26a41c466f9bac4adbc5ce219306a8b0c63a
Gyan Prakash <gyansir22@gmail.com>
2026-01-01 23:14:02 +0530

    Create 485.py

---
+++ linear_traversal/485.py
@@ -0,0 +1,18 @@
+#485. Max Consecutive Ones
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        current = 0 # setting the current sum to zero 
+        max_count = 0 # maximum count of 1s
+        for i in nums : # traversing through all elements of list  
+            if i == 1: 
+                current += 1 # incrementing the current counter if the element is 1 
+                max_count = max(current,max_count)  # ggetting maximum of current sum and max sum and updating the max sum 
+
+            else :
+                current = 0 # current counter to zero if the element is 0 
+
+        return max_count
+            
+            
+
+        
