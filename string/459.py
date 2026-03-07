85f474e0337868132c317730a6ea7c9ff5a0345b
Gyan Prakash <gyansir22@gmail.com>
2026-03-07 21:13:00 +0530

    Create 459.py

---
+++ string/459.py
@@ -0,0 +1,13 @@
+# 459. Repeated Substring Pattern
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        n = len(s)
+
+        for i in range(1,n//2+1):
+            if n % i == 0 :
+                sub = s[:i]
+                if sub*(n//i) ==s :
+                    return True 
+
+        return False 
+        
